a08e5f2e9146a39f7ebbe973d791303a7bd3d7de
danivelarde <danielavelarde4@gmail.com>
2022-06-01 16:45:36 -0400

    navbar and header basics

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import Navbar from './components/Navbar';
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <><Navbar />
+    <Header /></>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+    <div className="main-info">
+    <h1>DOWN THE ISLE</h1>
+    <div className="main-buttons">
+    <a href="https://drive.google.com/file/d/1Xkj__VzoBeI0kfXyG9hW1QHKD_r4pIjk/view?usp=sharing"  className="btn-start bg-black text-white" target="_blank" rel="noopener noreferrer">Let's go</a>
+    <a href="https://drive.google.com/file/d/1Xkj__VzoBeI0kfXyG9hW1QHKD_r4pIjk/view?usp=sharing"  className="btn-find bg-black text-white" target="_blank" rel="noopener noreferrer">Find a couple</a>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
