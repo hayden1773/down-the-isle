8cb91ce0ea92488fbc911a4c194f9e8e754af1de
Kristina Pang <kvypang@gmail.com>
2022-06-02 11:19:27 -0700

    about us page layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,8 @@
 import React from 'react'
-// import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 function About() {
     return (
-        <h1>About Us</h1>
-    //     <div className="container">
-    //         <MDBRow>
-    //   <MDBCol lg='4' md='12' className='mb-4'>
-    //     <img src='https://mdbootstrap.com/img/new/standard/city/047.webp' className='img-fluid rounded' alt='' />
-    //   </MDBCol>
 
-    //   <MDBCol lg='4' md='6' className='mb-4'>
-    //     <img
-    //       src='https://mdbootstrap.com/img/new/standard/city/047.webp'
-    //       className='img-fluid rounded-circle'
-    //       alt=''
-    //     />
-    //   </MDBCol>
-
-    //   <MDBCol lg='4' md='6' className='mb-4'>
-    //     <img
-    //       src='https://mdbootstrap.com/img/new/standard/city/047.webp'
-    //       className='img-fluid rounded-pill'
-    //       alt=''
-    //     />
-    //   </MDBCol>
-    // </MDBRow>
   );
 }
 
