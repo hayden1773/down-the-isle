da0b155c503eb4f9f59533e58e8904c617a61f36
Kristina Pang <kvypang@gmail.com>
2022-06-02 11:02:52 -0700

    about us page route and component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
   return (
-    <><Navbar />
-    <Header /></>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+    <Route path='/' element={<Header/>}/>
+    <Route path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react"
 import logo from "../components/logo.png"
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white text-black">
@@ -12,13 +14,13 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
         <li className="nav-item ">
-          <a className="nav-link " aria-current="page" href="#">Home</a>
+          <Link to='/' className="nav-link " aria-current="page">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link " aria-current="page" href="#">Login</a>
+          <Link to='/login' className="nav-link " aria-current="page">Login</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link " href="#">About us</a>
+          <Link to='/about' className="nav-link ">About us</Link>
         </li>
         
       </ul>
