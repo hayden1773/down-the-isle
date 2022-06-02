805fb6575eed78ea3ab862241f0904f91ef55ceb
danivelarde <danielavelarde4@gmail.com>
2022-06-02 13:03:30 -0400

    navheadupdates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,13 +12,13 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
         <li className="nav-item ">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link " aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Login</a>
+          <a className="nav-link " aria-current="page" href="#">Login</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Get Started</a>
+          <a className="nav-link " href="#">About us</a>
         </li>
         
       </ul>
