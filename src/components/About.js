d5e260d4eb3751ea49d558d9e22f8f18c2fdec0f
Kristina Pang <kvypang@gmail.com>
2022-06-02 19:20:42 -0700

    code cleanup

--- src/components/About.js
+++ src/components/About.js
@@ -20,42 +20,6 @@ function About() {
           <p>Text 4</p>
           </ul>
       </div></div>
-
-/* <div className="row row-cols-1 row-cols-md-2" id="aboutCards">
-  <div className="col mb-4">
-    <div className="card text-center p-3 m-5" id="img1">
-      <img src="https://dragonflytraveller.co.uk/sites/default/files/weddings.jpg" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Easier access to venues!</h5>
-      </div>
-    </div>
-  </div>
-  <div className="col mb-4">
-    <div className="card text-center p-3" id="img2">
-      <img src="https://media.snacksafely.com/wp-content/uploads/2015/09/airplane-flight-sunset-1068x668.jpg" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Plan your entire odyssey</h5>
-      </div>
-    </div>
-  </div>
-  <div className="col mb-4">
-    <div className="card text-center p-3" id="img3">
-      <img src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Share your registry</h5>
-      </div>
-    </div>
-  </div>
-  <div className="col mb-4">
-    <div className="card text-center p-3" id="img4">
-      <img src="https://upload.oola.com/rola/uploads/2021/12/wedding-puzzledd1bdc-1.jpg" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Manage your guest list</h5>
-      </div>
-    </div>
-  </div>
-</div> */
-
   );
 }
 
