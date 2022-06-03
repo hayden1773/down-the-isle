71c7270068ed4f77ba14e31b325e0b64fdf34145
Kristina Pang <kvypang@gmail.com>
2022-06-02 19:19:29 -0700

    page layout with images

--- src/components/About.js
+++ src/components/About.js
@@ -1,54 +1,61 @@
-import React from 'react'
+import React from "react"
 
 function About() {
     return (
-<div className="jumbotron m-0" id="portfolio">
-    <div className="row m-3">
-      <div className="headerPort text-center pb-5 mt-0">
-      <h1>WHY CHOOSE US</h1>
-      </div>
-    <div className="col-sm-6">
-      <div className="card">
-        <div className="card-body bg1 text-white  font-weight-bold">
-          <h5 className="card-title">E-COMMERCE BACKEND APP</h5>
-          <p className="card-text">Try it in Insomia, you won't regret</p>
-         
-        </div>
+
+<div className="container" id="aboutUs">
+        <h1>Why Choose Us?</h1>
+        <br/>
+         <br/>
+        <div className="container" id="about">
+        <ul><img className="rounded-circle image1" alt="img1" src="https://withjoy.com/blog/wp-content/uploads/2021/07/Photo-1-1170x781.jpg"/>
+        <p>Text 1</p>
+         <img className="rounded-circle image1" alt="img1" src="https://media.snacksafely.com/wp-content/uploads/2015/09/airplane-flight-sunset-1068x668.jpg"/>
+         <p>Text 2</p>
+         <br/>
+         <br/>
+          <img className="rounded-circle image1" alt="img1" src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg"/>
+          <p>Text 3</p>
+          <img className="rounded-circle image1" alt="img1" src="https://upload.oola.com/rola/uploads/2021/12/wedding-puzzledd1bdc-1.jpg"/>
+          <p>Text 4</p>
+          </ul>
+      </div></div>
+
+/* <div className="row row-cols-1 row-cols-md-2" id="aboutCards">
+  <div className="col mb-4">
+    <div className="card text-center p-3 m-5" id="img1">
+      <img src="https://dragonflytraveller.co.uk/sites/default/files/weddings.jpg" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Easier access to venues!</h5>
       </div>
     </div>
-    <div className="col-sm-6">
-      <div className="card">
-        <div className="card-body bg2 text-white font-weight-bold">
-          <h5 className="card-title">LANDMARKD- MY TRAVEL SITE!</h5>
-          <p className="card-text">Tired of regular travel sites? TRY THIS!</p>
-         
-        </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card text-center p-3" id="img2">
+      <img src="https://media.snacksafely.com/wp-content/uploads/2015/09/airplane-flight-sunset-1068x668.jpg" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Plan your entire odyssey</h5>
       </div>
     </div>
-   
-      <div className="flex-column text-white text-right" id="users" ></div>  
-      <div className="col-sm-6">
-        <div className="card">
-          <div className="card-body bg3 text-white font-weight-bold">
-            <h5 className="card-title">READ ME GENERATOR</h5>
-            <p className="card-text">Generate your Read Me the easiest way!</p>
-           
-          </div>
-        </div>
-      </div>      
-    
-      <div className="col-sm-6">
-        <div className="card justify-content-center">
-          <div className="card-body bg4 text-white font-weight-bold">
-            <h5 className="card-title">TECHBLOG</h5>
-            <p className="card-text">Don't forget to leave your reviews!</p>
-          
-            
-          </div>
-        </div>
-      </div>    
-      </div>     
+  </div>
+  <div className="col mb-4">
+    <div className="card text-center p-3" id="img3">
+      <img src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Share your registry</h5>
       </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card text-center p-3" id="img4">
+      <img src="https://upload.oola.com/rola/uploads/2021/12/wedding-puzzledd1bdc-1.jpg" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Manage your guest list</h5>
+      </div>
+    </div>
+  </div>
+</div> */
+
   );
 }
 
