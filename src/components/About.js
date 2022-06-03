1a3769deaa848ea44c8ba3a81cb59968b3a2e310
Kristina Pang <kvypang@gmail.com>
2022-06-02 19:23:30 -0700

    captions

--- src/components/About.js
+++ src/components/About.js
@@ -9,17 +9,18 @@ function About() {
          <br/>
         <div className="container" id="about">
         <ul><img className="rounded-circle image1" alt="img1" src="https://withjoy.com/blog/wp-content/uploads/2021/07/Photo-1-1170x781.jpg"/>
-        <p>Text 1</p>
+        <p>Easy access to venues!</p>
          <img className="rounded-circle image1" alt="img1" src="https://media.snacksafely.com/wp-content/uploads/2015/09/airplane-flight-sunset-1068x668.jpg"/>
-         <p>Text 2</p>
+         <p>Plan your etire adventure</p>
          <br/>
          <br/>
           <img className="rounded-circle image1" alt="img1" src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg"/>
-          <p>Text 3</p>
+          <p>Share your registry</p>
           <img className="rounded-circle image1" alt="img1" src="https://upload.oola.com/rola/uploads/2021/12/wedding-puzzledd1bdc-1.jpg"/>
-          <p>Text 4</p>
+          <p>Manage your guest list</p>
           </ul>
       </div></div>
+
   );
 }
 
