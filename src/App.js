f6d34f85441e90d9a407592d068d3637b8235438
Kristina Pang <kvypang@gmail.com>
2022-06-03 12:56:28 -0700

    pages added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar/index';
 import Header from './components/Header';
 import About from './components/About';
 
