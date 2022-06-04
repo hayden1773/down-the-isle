67a020bb0edb8cba865fefe2f7ea52647753e4db
Kristina Pang <kvypang@gmail.com>
2022-06-04 12:49:41 -0700

    dashboard pages

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 function Dashboard () {
   return (
     <div className="UserDash">
       <h1>Dashboard</h1>
-    <button className="createInvite">Create an Account</button>
+      <Link to="/createinvite"><button className="createInvite">Create an Invite</button></Link>
+      <Link to="/viewinvite"><button className="viewInvite">View Invite</button></Link>
+      <Link to="/itinerary"><button className="Itinerary">View Itinerary</button></Link>
+      <Link to="/guestlist"><button className="guestList">Guest List</button></Link>
 
 
     </div>
