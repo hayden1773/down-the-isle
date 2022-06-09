4b1d24ea9f89597a85726afe7a6cd711cff2b755
danivelarde <danielavelarde4@gmail.com>
2022-06-09 18:20:27 -0400

    last changes

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     
     <nav className="navbar navbar-expand-lg navbar-light text-white">
     <div className="container-fluid">
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
