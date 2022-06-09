d97823fa585e8603e24010953ad8b9c294a28fff
Kristina Pang <kvypang@gmail.com>
2022-06-09 14:19:14 -0700

    styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,13 +10,13 @@ function About() {
             <br/>
             <div className="container" id="about">
                 <ul><img className="rounded-circle image1" alt="img1" src="https://cdn0.weddingwire.com/vendor/982580/3_2/960/jpg/details0012_51_85289-157895752845788.jpeg"/>
-                    <p>Provide all the details of your special day in one place</p>
+                    <p>	&#10084; Provide all the details of your special day in one place</p>
                     <img className="rounded-circle image1" alt="img1" src="https://i.etsystatic.com/7739037/r/il/cc1981/3025588238/il_794xN.3025588238_abjr.jpg"/>
-                    <p>Manage your Guest List</p>
+                    <p> &#10084; Manage your Guest List</p>
                     <img className="rounded-circle image1" alt="img1" src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg"/>
-                    <p>Share your invites</p>
+                    <p>	&#10084; Share your invites</p>
                     <img className="rounded-circle image1" alt="img1" src="https://www.stylishwedd.com/media/catalog/product/cache/730028fce2d3fb3e70bdf76b1c05350a/r/o/romantic-pink-floral-layered-wedding-invitations-swpi075-1.jpg"/>
-                    <p>Manage your invitations</p>
+                    <p>	&#10084; Manage your invitations</p>
                 </ul>
             </div>
         </div>
