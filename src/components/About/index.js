8155e0f614ee8504ad382ac17d5ad81a0bf398db
Kristina Pang <kvypang@gmail.com>
2022-06-09 14:05:40 -0700

    updated all images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,13 +9,13 @@ function About() {
             <h3>Here at Down-the-Isle, we offer couples a simplistic, and stress-free way to create and send invitations for guests of their celebration of love.</h3>
             <br/>
             <div className="container" id="about">
-                <ul><img className="rounded-circle image1" alt="img1" src="https://withjoy.com/blog/wp-content/uploads/2021/07/Photo-1-1170x781.jpg"/>
+                <ul><img className="rounded-circle image1" alt="img1" src="https://cdn0.weddingwire.com/vendor/982580/3_2/960/jpg/details0012_51_85289-157895752845788.jpeg"/>
                     <p>Provide all the details of your special day in one place</p>
-                    <img className="rounded-circle image1" alt="img1" src="https://media.snacksafely.com/wp-content/uploads/2015/09/airplane-flight-sunset-1068x668.jpg"/>
+                    <img className="rounded-circle image1" alt="img1" src="https://i.etsystatic.com/7739037/r/il/cc1981/3025588238/il_794xN.3025588238_abjr.jpg"/>
                     <p>Manage your Guest List</p>
                     <img className="rounded-circle image1" alt="img1" src="https://media.guestofaguest.com/t_article_content/wp-content/uploads/2013/10/wedding-gift-table.jpg"/>
                     <p>Share your invites</p>
-                    <img className="rounded-circle image1" alt="img1" src="https://upload.oola.com/rola/uploads/2021/12/wedding-puzzledd1bdc-1.jpg"/>
+                    <img className="rounded-circle image1" alt="img1" src="https://www.stylishwedd.com/media/catalog/product/cache/730028fce2d3fb3e70bdf76b1c05350a/r/o/romantic-pink-floral-layered-wedding-invitations-swpi075-1.jpg"/>
                     <p>Manage your invitations</p>
                 </ul>
             </div>
