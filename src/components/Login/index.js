cb2a84296c4baf96b67a77ad337ff6efca7dd58e
danivelarde <danielavelarde4@gmail.com>
2022-06-10 15:32:56 -0400

    accepting coming changes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -24,7 +24,7 @@ export default function Login({
 
   const signupSubmit = (e) => {
     e.preventDefault();
-    console.log("seval is here to marry", signupData)
+    console.log("seval is married", signupData)
     signup(signupData);
     
     setSignupData({
