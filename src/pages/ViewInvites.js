bf264b3c478881c418d405283848d40e77a28e5b
Kristina Pang <kvypang@gmail.com>
2022-06-11 10:23:50 -0700

    cleanup invites page

--- src/pages/ViewInvites.js
+++ src/pages/ViewInvites.js
@@ -1,12 +1,8 @@
 import React from 'react'
-
 // import Navbar from '../components/NavBar/index'
 import InvitationPreview from '../components/InvitationPreview/index'
 import PreviewData from '../components/PreviewData/previewData'
-import PreviewItinerary from '../components/PreviewItinerary/previewItinerary'
 //import {useState} from 'react'
-
-
 
 //const[token,setToken]=useState(null)
 
@@ -21,7 +17,6 @@ const Invite = ({userId, setUserId,setToken,token}) => {
     <div>
       {/* <Navbar /> */}
       <PreviewData userId={userId} setUserId={setUserId} setToken={findToken} token={token} /> 
-      {/* <PreviewItinerary /> */}
       <InvitationPreview />
       
       {/* <Footer /> */}
